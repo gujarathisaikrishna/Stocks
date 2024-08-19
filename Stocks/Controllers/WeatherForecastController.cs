7ecee93a5e90cd2d814b1c497514ff193995d225
gujarathis <gujarathis@QASPXDVGR274>
2024-08-19 19:53:50 +0530

    Add project files.

---
+++ Stocks/Controllers/WeatherForecastController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Stocks.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+       
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IWeatherForecastService _weatherForecastService;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IWeatherForecastService weatherForecastService)
+        {
+            _logger = logger;
+            _weatherForecastService = weatherForecastService;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            var result = _weatherForecastService.Get();
+            return result;
+        }
+    }
+}
